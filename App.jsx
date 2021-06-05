cd8b3083ee82a112f74ddaa506a8dca0da73c7ea
Neet Patel <66862999+PatelNeet@users.noreply.github.com>
2021-06-05 14:35:33 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+// import "./styles.css";
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App() {
+  return (
+    <div>
+      {/* <h1>Hii</h1> */}
+      {/* components */}
+      <Header />
+      <Note />
+      <Note />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
